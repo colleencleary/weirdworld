56c7c31f5b1e3b77688f2d30e46ac6518b6a1eb4
colleencleary <cleary.ce@gmail.com>
2018-08-10 21:04:04 -0400

    started comments model

--- app/models/attraction.rb
+++ app/models/attraction.rb
@@ -1,12 +1,12 @@
 class Attraction
-    DB = PG.connect(host: "localhost", port: 5432, dbname: 'weirdworld_development')
+    # DB = PG.connect(host: "localhost", port: 5432, dbname: 'weirdworld_development')
 
-   #  if(ENV[‘DATABASE_URL’])
-   #     uri = URI.parse(ENV[‘DATABASE_URL’])
-   #     DB = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
-   # else
-   #     DB = PG.connect(host: “localhost”, port: 5432, dbname: ‘weirdworld_development’)
-   # end
+    if(ENV['DATABASE_URL'])
+       uri = URI.parse(ENV['DATABASE_URL'])
+       DB = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
+   else
+       DB = PG.connect(host: "localhost", port: 5432, dbname: 'weirdworld_development')
+   end
 
   def self.all
      results = DB.exec("SELECT * FROM attractions")
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,26 @@
 class Comment
+  if(ENV['DATABASE_URL'])
+    uri = URI.parse(ENV['DATABASE_URL'])
+    DB = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
+  else
+    DB = PG.connect(host: "localhost", port: 5432, dbname: 'weirdworld_development')
+  end
 
   def self.all
-    results = DB.exec()
+    results = DB.exec(
+      <<-SQL
+      SELECT * FROM comments;
+      SQL
+    )
   end
 
   def self.find(id)
-    results = DB.exec()
+    results = DB.exec(
+      <<-SQL
+      SELECT * FROM comments
+      where id=#{id};
+      SQL
+    )
   end
 
   def self.create(opts)
