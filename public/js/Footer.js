5034d95ece39f69aa075a214bab1bf6412392b18
acroak <acroak@live.com>
2018-08-14 16:05:23 -0400

    footer

--- public/js/Footer.js
+++ public/js/Footer.js
@@ -2,6 +2,9 @@ class Footer extends React.Components {
   render(){
     return(
       <footer>
+        <h4>&copy; Copyright 2018</h4>
+        <h4>Created with &hearts; for <a href="https://generalassemb.ly/">GA WDIR-ADI</a></h4>
+        <a href="#"><h5> About the Coders </h5></a>
       </footer>
     )
   }
