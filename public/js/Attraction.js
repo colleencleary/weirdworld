97ef5f10c5d163b79d536d573130e6e33472b594
ravenlee094 <ravenlee0327@gmail.com>
2018-08-14 16:13:30 -0400

    attraction styling

--- public/js/Attraction.js
+++ public/js/Attraction.js
@@ -4,20 +4,27 @@ class Attraction extends React.Component {
       let comments = this.props.attraction.comments
     return (
       <div>
-      <button onClick={()=> this.props.toggleState("attractionListIsVisible", "attractionIsVisible")}>Back</button>
+        <div class="background">
 
-          <h3><span>Attraction Name:</span> {this.props.attraction.name}</h3>
-          <p><span>Submitted By:</span> {this.props.attraction.submitted_by}</p>
-          <p>{this.props.attraction.city}, {this.props.attraction.country}</p>
-          <p><span>Website:</span> {this.props.attraction.website}</p>
-          <p><span>Rating:</span> {this.props.attraction.rating}</p>
+          <p class="name">{this.props.attraction.name}</p>
+
+          <p class="title">{this.props.attraction.city}, {this.props.attraction.country}</p>
+
+          <p class="title">{this.props.attraction.website}</p>
+
+          <p class="title"><span>Rating:</span> {this.props.attraction.rating}</p>
+
+          <p class="title"><span>Submitted By:</span> {this.props.attraction.submitted_by}</p>
 
           <img src={this.props.attraction.image} alt={this.props.attraction.name} />
-          <p><span>Description:</span> {this.props.attraction.description}</p>
 
-          <p><span>Tags:</span> {this.props.attraction.tags}</p>
+          <p class="title">{this.props.attraction.description}</p>
+
+          <p class="title"><span>Tags:</span> {this.props.attraction.tags}</p>
+          </div>
+
           <div>
-             <p>Comments:{JSON.stringify(comments)}</p>
+             <p class="title">Comments:{JSON.stringify(comments)}</p>
           </div>
           <div>
             <AttractionForm attraction={this.props.attraction} handleSubmit={this.props.handleSubmit}/>
